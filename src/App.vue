f94daa9b9f4b656a2ba8d6befd5271aaccdb128b
lucasDevLepidus <lucasanselmo@lepidus.com.br>
2022-09-07 18:50:37 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,154 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text900">Texto 900</h1>
+    <h1 class="text900Italic">Texto 900 - itálico</h1>
+
+    <h2 class="text800">Texto 800</h2>
+    <h2 class="text800Italic">Texto 800 - itálico</h2>
+
+    <h3 class="text700">Texto 700</h3>
+    <h3 class="text700Italic">Texto 700 - itálico</h3>
+
+    <h3 class="text600">Texto 600</h3>
+    <h3 class="text600Italic">Texto 600 - itálico</h3>
+
+    <h3 class="text500">Texto 500</h3>
+    <h3 class="text500Italic">Texto 500 - itálico</h3>
+
+    <h3 class="text400">Texto 400</h3>
+    <h3 class="text400Italic">Texto 400 - itálico</h3>
+
+    <h4 class="text300">Texto 300</h4>
+    <h4 class="text300Italic">Texto 300 - itálico</h4>
+
+    <h5 class="text200">Texto 200</h5>
+    <h5 class="text200Italic">Texto 200 - itálico</h5>
+
+    <h6 class="text100">Texto 100</h6>
+    <h6 class="text100Italic">Texto 100 - itálico</h6>
+  </div>
+</template>
+
+<style scoped>
+  .container{
+    margin-top: 10vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .text900{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 900;
+    font-size: 2rem;
+  }
+
+  .text900Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 900;
+    font-style: italic;
+    font-size: 2rem;
+  }
+
+  .text800{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 800;
+    font-size: 1.875rem;
+  }
+  .text800Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 800;
+    font-style: italic;
+    font-size: 1.875rem;
+  }
+
+  .text700{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 700;
+    font-size: 1.75rem;
+  }
+  .text700Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 700;
+    font-style: italic;
+    font-size: 1.75rem;
+  }
+
+  .text600{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 600;
+    font-size: 1.625rem;
+  }
+  .text600Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 600;
+    font-style: italic;
+    font-size: 1.625rem;
+  }
+
+  .text500{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 500;
+    font-size: 1.5rem;
+  }
+  .text500Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 500;
+    font-style: italic;
+    font-size: 1.5rem;
+  }
+
+  .text400{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 400;
+    font-size: 1.375rem;
+  }
+  .text400Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 400;
+    font-style: italic;
+    font-size: 1.375rem;
+  }
+
+  .text300{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 300;
+    font-size: 1.25rem;
+  }
+  .text300Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 300;
+    font-style: italic;
+    font-size: 1.25rem;
+  }
+
+  .text200{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 200;
+    font-size: 1.125rem;
+  }
+  .text200Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 200;
+    font-style: italic;
+    font-size: 1.125rem;
+  }
+
+  .text100{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 100;
+    font-size: 1rem;
+  }
+  .text100Italic{
+    font-family: 'Noto Sans', sans-serif;
+    font-weight: 100;
+    font-style: italic;
+    font-size: 1rem;
+  }
+
+</style>
